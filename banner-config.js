5b19622d896aec8bdf3b250f9ecdb775b1f812e9
iamch094-jpg <iamch094@gmail.com>
2026-07-15 21:07:31 +0900

    홈페이지 및 관리자 페이지 업데이트

---
+++ banner-config.js
@@ -0,0 +1,15 @@
+/* 관리자 페이지의 GitHub 저장 버튼이 이 파일을 자동으로 수정합니다. */
+window.HANOK_BANNERS = [
+  {
+    id: 'opening', enabled: true, size: 'medium', icon: '🏡',
+    image: 'images/banner-event.jpg', title: '한옥스테이 오픈 이벤트',
+    message: '고즈넉한 한옥에서 만나는 온전한 쉼',
+    buttonEnabled: true, buttonText: '예약 문의하기', link: '#booking'
+  },
+  {
+    id: 'pool', enabled: true, size: 'medium', icon: '🏊',
+    image: 'images/banner-pool.jpg', title: '프라이빗 수영장',
+    message: '한옥에서 즐기는 여유로운 물놀이',
+    buttonEnabled: true, buttonText: '자세히 보기', link: '#gallery'
+  }
+];
