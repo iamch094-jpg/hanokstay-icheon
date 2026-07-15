d3f96a2ccc3fbce2eba314e14c1b7b40f4fffe38
iamch094-jpg <iamch094@gmail.com>
2026-07-15 21:49:53 +0900

    Update website banners from admin page

--- banner-config.js
+++ banner-config.js
@@ -1,15 +1,27 @@
-/* 관리자 페이지의 GitHub 저장 버튼이 이 파일을 자동으로 수정합니다. */
+/* 한옥스테이 더 이천 배너 설정 */
 window.HANOK_BANNERS = [
   {
-    id: 'opening', enabled: true, size: 'medium', icon: '🏡',
-    image: 'images/banner-event.jpg', title: '한옥스테이 오픈 이벤트',
-    message: '고즈넉한 한옥에서 만나는 온전한 쉼',
-    buttonEnabled: true, buttonText: '예약 문의하기', link: '#booking'
+    "id": "opening",
+    "enabled": true,
+    "size": "medium",
+    "icon": "🏡",
+    "image": "images/banner-event.jpg",
+    "title": "한옥스테이 오픈 이벤트",
+    "message": "고즈넉한 한옥에서 만나는 온전한 쉼",
+    "buttonEnabled": true,
+    "buttonText": "예약 문의하기",
+    "link": "#booking"
   },
   {
-    id: 'pool', enabled: true, size: 'medium', icon: '🏊',
-    image: 'images/banner-pool.jpg', title: '프라이빗 수영장',
-    message: '한옥에서 즐기는 여유로운 물놀이',
-    buttonEnabled: true, buttonText: '자세히 보기', link: '#gallery'
+    "id": "pool",
+    "enabled": true,
+    "size": "medium",
+    "icon": "🏊",
+    "image": "images/banner-pool.jpg",
+    "title": "프라이빗 수영장",
+    "message": "한옥에서 즐기는 여유로운 물놀이",
+    "buttonEnabled": true,
+    "buttonText": "자세히 보기",
+    "link": "#gallery"
   }
 ];
